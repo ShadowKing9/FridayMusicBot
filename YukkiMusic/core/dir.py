044bc21801d2f805f91ab3de09f6d11676a2f95b
ShadowKing9 <88343817+ShadowKing9@users.noreply.github.com>
2022-07-10 23:10:45 +0530

    Update dir.py

--- YukkiMusic/core/dir.py
+++ YukkiMusic/core/dir.py
@@ -3,7 +3,7 @@
 #
 # This file is part of < https://github.com/MasterShad0w/FridayMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/MasterShad0w/FridayMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 
